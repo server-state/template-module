a2f25e2e64bdf75855e6ebf65b46bbb3a4cdac99
Ludwig Richter <richter@fliegwerk.com>
2019-09-04 15:26:16 +0200

    Changed gitignore

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * A module for the server-state system
+ * @returns A JSON-serializable (via `JSON.stringify()`) version information about the server state
+ */
+module.exports = async function() {
+	// TODO: Implement module
+	return 'Module loaded';
+};
