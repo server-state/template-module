f38f362e3e7209d9b413431cf5441fe1b0bda327
Pablo Klaschka <contact@pabloklaschka.de>
2019-09-06 11:31:14 +0200

    Fixed linting issues

--- tests/001-basic.test.js
+++ tests/001-basic.test.js
@@ -1,12 +1,12 @@
 test('two plus two is four', () => {
-	expect(2 + 2).toBe(4);
+    expect(2 + 2).toBe(4);
 });
 
 describe('Validity of return value of function', () => {
-	test('JSON serializable', async (done) => {
-		const res = await (require('../src/')());
+    test('JSON serializable', async (done) => {
+        const res = await (require('../src/')());
 
-		expect(JSON.stringify(res)).toBeTruthy();
-		done();
-	});
+        expect(JSON.stringify(res)).toBeTruthy();
+        done();
+    });
 });
